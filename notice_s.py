b0e3ce4695c0a6e752a1d12502b19d0a65691caf
Baris Dincer <81470496+BrsDincer@users.noreply.github.com>
2021-12-03 17:49:08 +0300

    Create notice_s.py

---
+++ notice_s.py
@@ -0,0 +1,28 @@
+def show_notice():
+    
+    log_ipp_v = """
+    
+     IIIIIIIIIIIIIIIIIIII        PPPPPPPPPPPPPPPPP        VVVVVVVV           VVVVVVVV
+     I::::::::II::::::::I        P::::::::::::::::P       V::::::V           V::::::V
+     I::::::::II::::::::I        P::::::PPPPPP:::::P      V::::::V           V::::::V
+     II::::::IIII::::::II        PP:::::P     P:::::P     V::::::V           V::::::V
+       I::::I    I::::I            P::::P     P:::::P      V:::::V           V:::::V 
+       I::::I    I::::I            P::::P     P:::::P       V:::::V         V:::::V  
+       I::::I    I::::I            P::::PPPPPP:::::P         V:::::V       V:::::V   
+       I::::I    I::::I            P:::::::::::::PP           V:::::V     V:::::V    
+       I::::I    I::::I            P::::PPPPPPPPP              V:::::V   V:::::V     
+       I::::I    I::::I            P::::P                       V:::::V V:::::V      
+       I::::I    I::::I            P::::P                        V:::::V:::::V       
+       I::::I    I::::I            P::::P                         V:::::::::V        
+     II::::::IIII::::::II        PP::::::PP                        V:::::::V         
+     I::::::::II::::::::I ...... P::::::::P                         V:::::V          
+     I01000110II00110100I .::::. P01000110P                          V:::V           
+     IIIIIIIIIIIIIIIIIIII ...... PPPPPPPPPP                           VVV 
+    
+    
+    """
+    
+    Message_War = "When using this tool, please make sure to use VPN or take additional precautions to hide your IP identity"
+    
+    print(log_ipp_v)
+    print("\032[1;36m%s\x1b[0m" % (Message_War))
