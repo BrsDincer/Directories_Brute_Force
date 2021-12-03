f1c4b943e5b5acda3bf448296627d7a74f7fa13c
Baris Dincer <81470496+BrsDincer@users.noreply.github.com>
2021-12-03 17:49:30 +0300

    Create con_h_n.py

---
+++ con_h_n.py
@@ -0,0 +1,49 @@
+def cen_notice():
+    
+    log_ipp_v = """
+    
+     IIIIIIIIIIIIIIIIIIII        PPPPPPPPPPPPPPPPP        VVVVVVVV           VVVVVVVV
+     I::::::::II::::::::I        P::::::::::::::::P       V::::::V           V::::::V
+     I::::::::II::::::::I        P::::::PPPPPP:::::P      V::::::V           V::::::V
+     II::::::IIII::::::II        PP:::::P     P:::::P     V::::::V           V::::::V
+       I::::I    I::::I            P::::P     P:::::P      V:::::V           V:::::V 
+       I::::I    I::::I            P::::P     P:::::P       V:::::V         V:::::V  
+       I::::I    I::::I            P::::PPPPPP:::::P         V:::::V       V:::::V   
+       I::::I    I::::I            P:::::::::::::PP           V:::::V     V:::::V    
+       I::::I    I::::I            P::::PPPPPPPPP              V:::::V   V:::::V     
+       I::::I    I::::I            P::::P                       V:::::V V:::::V      
+       I::::I    I::::I            P::::P                        V:::::V:::::V       
+       I::::I    I::::I            P::::P                         V:::::::::V        
+     II::::::IIII::::::II        PP::::::PP                        V:::::::V         
+     I::::::::II::::::::I ...... P::::::::P                         V:::::V          
+     I01000110II00110100I .::::. P01000110P                          V:::V           
+     IIIIIIIIIIIIIIIIIIII ...... PPPPPPPPPP                           VVV 
+    
+    
+    """
+    Message_Note = """
+    
+    
+    The censorship control application takes place in 3 stages.
+    
+    [NOTED]
+    
+    This process may differ depending on your internet connection and permissions.
+    If you are getting an additional error, please check your settings and permissions.
+    
+    PROXY WILL BE SELECTED RANDOMLY (for 2 - 3), PLEASE MAKE SURE YOU ARE USING VPN
+
+    1) QUICK CONTROL WITH STANDARD HTTPS METHODS
+    
+    2) ACCESSIBILITY CHECK WITH VARIOUS PROXY CONNECTIONS (CAN TAKE A LONG TIME)
+    
+    3) HTTPS METHOD CONTROL WITH PROXY CONNECTIONS (CAN TAKE A LONG TIME)
+    
+    [!] '/' FOR EXIT
+    
+    PLEASE CONTINUE PROCESSING BY ENTERING THE NUMBER CORRECTING THE METHODS
+
+    
+    """
+    print(log_ipp_v)
+    print("\032[1;36m%s\x1b[0m" % (Message_Note))
